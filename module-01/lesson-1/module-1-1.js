3515d3279bb3997a16e380f4413fcecd0b8477db
lirikape <lirikape@gmail.com>
2023-08-30 16:14:45 +0300

    Update module-1-1.js

--- module-01/lesson-1/module-1-1.js
+++ module-01/lesson-1/module-1-1.js
@@ -109,3 +109,32 @@
 // const isAdult = confirm('Вам є 18?');
 
 // console.log(isAdult);
+
+//! 
+// console.log('true:>> ', !true);
+
+// console.log("" || null || undefined);
+// console.log(true && "0" && "10");
+
+// console.log(true && 3); // 
+// console.log(false && 3); // 
+// console.log(true && 4 && 'kiwi'); // 
+// console.log(true && 0 && 'kiwi'); // 
+// console.log(true || 3); // 
+// console.log(true || 3 || 4); // 
+// console.log(true || false || 7); // 
+// console.log(null || 2 || undefined); // 
+// console.log((1 && null && 2) > 0); // 
+// console.log(null || (2 && 3) || 4); //
+
+// console.log(2 + 5 + ' ' + 1 + 0); 
+// console.log('' + 1 - '1');
+
+// console.log(true + false); 
+// console.log(6 / '3'); 
+// console.log('2' * '3'); 
+// console.log(4 + 5 + 'px'); 
+// console.log('$' + 4 + 5); 
+// console.log('4' - 2); 
+
+//  console.log('4px' - 2);
