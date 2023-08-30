8a1b635414239678ba5c8ba00136d89099d9a2dd
NikolayMosalov <137917653+NikolayMosalov@users.noreply.github.com>
2023-08-30 15:30:25 +0300

    Update module-1-1.js

--- module-01/lesson-1/module-1-1.js
+++ module-01/lesson-1/module-1-1.js
@@ -61,3 +61,51 @@
 
 //? Строга нерівність (!==)
 // console.log('4' !== 4);
+
+//! variebles
+
+/*
+  Примітивні типи даних: number, string, boolean, null та undefined.
+ */
+
+/*
+  Виведення даних: console.log та alert. Вкладка Console в Chrome Devtools.
+ */
+
+// console.log('Hello');
+// console.log('5');
+// console.log(5);
+
+// alert('Hello');
+// alert(5);
+
+/*
+  Змінні: оголошення let та const.
+ */
+
+// let userFirstName;
+
+// console.log(userFirstName);
+
+// userFirstName = 'Denis';
+
+// console.log(userFirstName);
+
+// const userLastName = 'Hunter';
+
+// console.log(userLastName);
+
+// userLastName = 'Carson';
+
+// console.log(userLastName);
+/*
+  Отримання даних: prompt та confirm.
+ */
+
+// const userFirstName = prompt('Ваше ім`я?');
+
+// console.log(userFirstName);
+
+// const isAdult = confirm('Вам є 18?');
+
+// console.log(isAdult);
