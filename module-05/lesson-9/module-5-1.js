80f064e940dc314179094947a305e4ea636b9293
Kreal11 <kirillshyrokov2@gmail.com>
2023-09-12 15:50:35 +0300

    task 4

--- module-05/lesson-9/module-5-1.js
+++ module-05/lesson-9/module-5-1.js
@@ -2,49 +2,15 @@
 // Створити об'єкт, який представляє користувача. У об'єкті повинні бути наступні поля: ім'я, прізвище, вік, email та метод, який виводить
 // повну інформацію про користувача.
 
-// const user = {
-//     userName: "Tom",
-//     lastName: "Ford",
-//     age: 34,
-//     email: "tom@gmail.com",
-
-//     showUser(){
-//         console.log(this)
-//     },
-
-//     modify(params, value){
-//         if(params !== "userName" && params !== "lastName"){
-//             return alert("Params are not valid");
-//         }
-        
-//         if(value[0] === value[0].toUpperCase() && value.length >= 3){
-//            return this[params] = value;
-//         }
-//         alert("Value are not valid!")
-//     },
-// };
-// user.modify("userName", "Rich");
-// user.showUser();
-
-//TODO:=========task-02=================
-// Для обьекта "user", написати метод для зміни ім'я або прізвища(змінити можна лише якесь з цих полів, змінювати або додавати нові
-//потрібно заборонити) з перевіркою на валідність даних(Перша літера має бути у верхньому реєстрі, довжина слова не менше 3 літер)
-
-
-
-//TODO:=========task-03=================
-// Створити глобальну функцію для обьекта "user", що може додавати властивості об'єкту, в контексті якого вона буде викликана. Додати цю функцію як метод об'єкта user, та додати за допомогою неї властивість friends із значенням:
-
-
-
 const user = {
     userName: "Tom",
     lastName: "Ford",
     age: 34,
     email: "tom@gmail.com",
 
-    showUser(){
-        console.log(this)
+    showUser(name = this.userName, age = this.age){
+      console.log(this);
+      console.log(`Hello, ${name}, you are ${age} y.o.`);
     },
 
     modify(params, value){
@@ -56,9 +22,19 @@ const user = {
            return this[params] = value;
         }
         alert("Value are not valid!")
-    }, 
-    
+    },
 };
+user.modify("userName", "Rich");
+user.showUser();
+
+//TODO:=========task-02=================
+// Для обьекта "user", написати метод для зміни ім'я або прізвища(змінити можна лише якесь з цих полів, змінювати або додавати нові
+//потрібно заборонити) з перевіркою на валідність даних(Перша літера має бути у верхньому реєстрі, довжина слова не менше 3 літер)
+
+
+
+//TODO:=========task-03=================
+// Створити глобальну функцію для обьекта "user", що може додавати властивості об'єкту, в контексті якого вона буде викликана. Додати цю функцію як метод об'єкта user, та додати за допомогою неї властивість friends із значенням:
 
 const addProperty = function (key, value) {
     this[key] = value;
@@ -90,7 +66,17 @@ user.addProperty("mood", "happy")
 console.log(user);
 
 //TODO:=========task-04=================
-//  Викликати метод user.info() таким чином, щоб він вивів результатом  ({name: 'Bob', lactName: 'Lasso',age: 50, email: 'BodLasso@gmail.com'})
+//  Викликати метод user.showUser() таким чином, щоб він вивів результатом  ({name: 'Bob', lastName: 'Lasso',age: 50, email: 'BodLasso@gmail.com'})
+
+const obj = {
+  userName: 'Bob',
+  lastName: 'Lasso',
+  age: 50,
+  email: 'BodLasso@gmail.com',
+};
+
+user.showUser.call(obj);
+
 
 //TODO:=========task-05=================
 //  Викличте функцію showFullName у контексті об'єкта user
