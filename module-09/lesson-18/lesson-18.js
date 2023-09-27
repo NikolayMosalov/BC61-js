9e3fd0b1316955360474affa9a7158349e71162b
nadiyniy <140396602+nadiyniy@users.noreply.github.com>
2023-09-27 14:46:39 +0300

    Task - 2

--- module-09/lesson-18/lesson-18.js
+++ module-09/lesson-18/lesson-18.js
@@ -25,41 +25,69 @@
  */
 
 // const getData = () =>
-//   new Promise((res) => {
-//     setTimeout(() => {
-//       const data = 1;
-//       console.log(data);
-//       res(data);
-//     }, 1000);
-//   });
+//     new Promise((res) => {
+//         setTimeout(() => {
+//             const data = 1;
+//             //   console.log(data);
+//             res(data);
+//         }, 1000);
+//     });
 
 // const getNewData = () =>
-//   new Promise((res) => {
-//     setTimeout(() => {
-//       const data = 2;
-//       console.log(data);
-//       res(data);
-//     }, 1000);
-//   });
+//     new Promise((res) => {
+//         setTimeout(() => {
+//             const data = 2;
+//             //   console.log(data);
+//             res(data);
+//         }, 1000);
+//     });
 
 // const getAnotherData = () =>
-//   new Promise((res) => {
-//     setTimeout(() => {
-//       const data = 3;
-//       console.log(data);
-//       res(data);
-//     }, 1000);
-//   });
+//     new Promise((res) => {
+//         setTimeout(() => {
+//             const data = 3;
+//             //   console.log(data);
+//             res(data);
+//         }, 1000);
+//     });
 
 // const getLastData = () =>
-//   new Promise((res) => {
-//     setTimeout(() => {
-//       const data = 4;
-//       console.log(data);
-//       res(data);
-//     }, 1000);
+//     new Promise((res) => {
+//         setTimeout(() => {
+//             const data = 4;
+//             //   console.log(data);
+//             res(data);
+//         }, 1000);
+//     });
+//? var - 1
+// const arr = [];
+
+// getData()
+//   .then((respons) => {
+//     arr.push(respons);
+
+//     return getNewData();
+//   })
+//   .then((respons) => {
+//     arr.push(respons);
+
+//     return getAnotherData();
+//   })
+//   .then((respons) => {
+//     arr.push(respons);
+
+//     return getLastData();
+//   })
+//   .then((respons) => {
+//     arr.push(respons);
+//     console.log(arr);
 //   });
 
+//? var - 2
+
+// Promise
+//     .all([getData(), getNewData(), getAnotherData(), getLastData()])
+//     .then(console.log);
 
 //TODO:====================03==========================
 /**
@@ -71,8 +99,6 @@
  * logCount повинна логувати кількість викликів
  */
 
-
-
 // countWithDelay(3000, 5, 1000);
 
 //TODO:====================04==========================
@@ -83,8 +109,6 @@
  * Якщо значення парне, вирішуй проміс і повертай "even" через 1 секунду.
  * Якщо значення не парне, вирішуй проміс і повертай "odd" через 2 секунди.
  */
-
-
 
 // const value = prompt('Paste value');
 // checkValue(value).then(console.log).catch(console.log);
