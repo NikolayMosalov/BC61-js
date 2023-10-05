25cee23b2350ac082d5ce49847678e0ab7b85d20
Apopkov1996 <antonpopkov113@gmail.com>
2023-10-05 15:56:00 +0300

    update onload

--- QA/QA-11-module/src/js/app.js
+++ QA/QA-11-module/src/js/app.js
@@ -4,12 +4,25 @@ import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import createGalleryCard from '../templates/gallery-card.hbs';
 
 const unsplashApi = new UnsplashAPI(12);
+const observer = new IntersectionObserver(
+  (entries, observer) => {
+    if (entries[0].isIntersecting) {
+      loadMoreData();
+    }
+  },
+  {
+    root: null,
+    rootMargin: '600px',
+    threshold: 1,
+  }
+);
 
 refs.form.addEventListener('submit', onSubmitForm);
 
 async function onSubmitForm(e) {
   e.preventDefault();
   const searchQuery = e.target.elements['user-search-query'].value.trim();
+  unsplashApi.page = 1;
 
   if (!searchQuery) {
     return Notify.failure('Your query is empty!=)');
@@ -24,9 +37,33 @@ async function onSubmitForm(e) {
 
     refs.list.innerHTML = createGalleryCard(response.results)
 
+    if (response.total > unsplashApi.perPage) {
+      observer.observe(refs.scrollTarget)
+    }
+
   } catch (error) {
     Notify.failure('Oppps, something wrong!');
   }
+};
+
+async function loadMoreData() {
+  unsplashApi.page += 1;
+
+  try {
+    const response = await unsplashApi.getPhotos();
+
+    refs.list.insertAdjacentHTML('beforeend', createGalleryCard(response.results));
+
+    if ( unsplashApi.page === response.total_pages) {
+      observer.unobserve(refs.scrollTarget)
+      Notify.info(`We're sorry, but you've reached the end of search results.`)
+    }
+
+  } catch (error) {
+    Notify.failure('Oppps, something wrong!');
+  }
+ 
+
 }
 
 
