2c1a38a032111f83a997330b87ec17bf0936eb83
Apopkov1996 <antonpopkov113@gmail.com>
2023-10-05 17:22:34 +0300

    update onLoadMore

--- QA/QA-11-module/src/js/app.js
+++ QA/QA-11-module/src/js/app.js
@@ -4,12 +4,10 @@ import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import createGalleryCard from '../templates/gallery-card.hbs';
 
 const unsplashApi = new UnsplashAPI(12);
-let firstLoad = false;
-
 
 const observer = new IntersectionObserver(
   (entries, observer) => {
-    if (entries[0].isIntersecting && firstLoad) {
+    if (entries[0].isIntersecting) {
       loadMoreData();
     }
   },
@@ -20,43 +18,45 @@ const observer = new IntersectionObserver(
   }
 );
 
-
-
 refs.form.addEventListener('submit', onSubmitForm);
 
 async function onSubmitForm(e) {
   e.preventDefault();
   const searchQuery = e.target.elements['user-search-query'].value.trim();
   unsplashApi.page = 1;
-  firstLoad = false;
 
   if (!searchQuery) {
     return Notify.failure('Your query is empty!=)');
-  }
+  };
+
 
   unsplashApi.query = searchQuery;
 
   try {
     const response = await unsplashApi.getPhotos();
 
+    console.log(response.results.length);
+    if (response.results.length === 0) {
+      Notify.warning(`No results ${searchQuery}`)
+      refs.list.innerHTML = '';
+      return;
+    };
+
     Notify.success(`We find ${response.total} images`);
 
-    refs.list.innerHTML = createGalleryCard(response.results)
+    refs.list.innerHTML = createGalleryCard(response.results);
 
-    // if (response.total > unsplashApi.perPage) {
-    //   observer.observe(refs.scrollTarget)
-    // }
-    observer.observe(refs.scrollTarget)
-    firstLoad = true;
-
+    if (response.total > unsplashApi.perPage) {
+      observer.observe(refs.scrollTarget)
+    };
   } catch (error) {
     Notify.failure('Oppps, something wrong!');
-  }
+  };
+
 };
 
 async function loadMoreData() {
   unsplashApi.page += 1;
-
 
   try {
     const response = await unsplashApi.getPhotos();
@@ -72,10 +72,34 @@ async function loadMoreData() {
 
   } catch (error) {
     Notify.failure('Oppps, something wrong!');
-  }
+  };
  
+};
 
-}
+
+// async function loadMoreData() {
+//   unsplashApi.page += 1;
+
+//   try {
+//     if (unsplashApi.page > 2) {
+//       const response = await unsplashApi.getPhotos();
+
+//       refs.list.insertAdjacentHTML(
+//         'beforeend',
+//         createGalleryCard(response.results)
+//       );
+
+//       if (unsplashApi.page === response.total_pages) {
+//         observer.unobserve(refs.scrollTarget);
+//         Notify.info(
+//           `We're sorry, but you've reached the end of search results.`
+//         );
+//       }
+//     }
+//   } catch (error) {
+//     Notify.failure('Oppps, something wrong!');
+//   }
+// }
 
 
 
