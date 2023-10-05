bcc915d9edd40be17811c24949b4b3f00860074c
nadiyniy <140396602+nadiyniy@users.noreply.github.com>
2023-10-05 15:32:03 +0300

    render markup

--- QA/QA-11-module/src/js/app.js
+++ QA/QA-11-module/src/js/app.js
@@ -1,5 +1,34 @@
 import { UnsplashAPI } from './unsplash-api';
+import refs from './refs';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import createGalleryCard from '../templates/gallery-card.hbs';
 
-const unsplashApi = new UnsplashAPI(20);
+const unsplashApi = new UnsplashAPI(12);
 
-unsplashApi.getPhotos().then(console.log);
+refs.form.addEventListener('submit', onSubmitForm);
+
+async function onSubmitForm(e) {
+  e.preventDefault();
+  const searchQuery = e.target.elements['user-search-query'].value.trim();
+
+  if (!searchQuery) {
+    return Notify.failure('Your query is empty!=)');
+  }
+
+  unsplashApi.query = searchQuery;
+
+  try {
+    const response = await unsplashApi.getPhotos();
+
+    Notify.success(`We find ${response.total} images`);
+
+    refs.list.innerHTML = createGalleryCard(response.results)
+
+  } catch (error) {
+    Notify.failure('Oppps, something wrong!');
+  }
+}
+
+
+
+
---
+++ QA/QA-11-module/src/js/refs.js
@@ -0,0 +1,6 @@
+const refs = {
+    form: document.querySelector('.js-search-form'),
+    list: document.querySelector('.js-gallery'),
+    scrollTarget: document.querySelector('.js-target-element')
+}
+export default refs;
