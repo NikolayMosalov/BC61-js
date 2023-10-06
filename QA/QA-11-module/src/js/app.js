b243ae1bde18990e6dcffb155bddd14b5ac705b6
Kreal11 <kirillshyrokov2@gmail.com>
2023-10-06 14:57:34 +0300

    updated scroll and new function loadMoreData

--- QA/QA-11-module/src/js/app.js
+++ QA/QA-11-module/src/js/app.js
@@ -59,44 +59,24 @@ async function onSubmitForm(e) {
 }
 
 async function loadMoreData() {
+  if (isFirstLoad) {
+    return (isFirstLoad = false);
+  }
   unsplashApi.page += 1;
-
   try {
     const response = await unsplashApi.getPhotos();
-
     refs.list.insertAdjacentHTML(
       'beforeend',
       createGalleryCard(response.results)
     );
-
     if (unsplashApi.page === response.total_pages) {
       observer.unobserve(refs.scrollTarget);
-      Notify.info(`We're sorry, but you've reached the end of search results.`);
+      Notify.info(`We’re sorry, but you’ve reached the end of search results.`);
     }
   } catch (error) {
     Notify.failure('Oppps, something wrong!');
   }
 }
-
-// async function loadMoreData() {
-//   if (isFirstLoad) {
-//     return (isFirstLoad = false);
-//   }
-//   unsplashApi.page += 1;
-//   try {
-//     const response = await unsplashApi.getPhotos();
-//     refs.list.insertAdjacentHTML(
-//       'beforeend',
-//       createGalleryCard(response.results)
-//     );
-//     if (unsplashApi.page === response.total_pages) {
-//       observer.unobserve(refs.scrollTarget);
-//       Notify.info(`We’re sorry, but you’ve reached the end of search results.`);
-//     }
-//   } catch (error) {
-//     Notify.failure('Oppps, something wrong!');
-//   }
-// }
 
 // async function loadMoreData() {
 //   try {
